825d1f78b558b7dcf2d0f9a112f652ae0fa52ab1
Shrijan Poudel <85357750+SlytherSavior@users.noreply.github.com>
2025-02-25 09:26:23 +0545

    easy spread props

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
 <!-- props in svelte-->
  <script>
     import Nested from './nested.svelte';
+    const question =  {
+        details : "3 * 7",
+        answer : " 21" ,
+        maker :'Shrijan'
+    };
  </script>
 
- <Nested  />
+ <Nested  {...question}/>
--- src/routes/nested.svelte
+++ src/routes/nested.svelte
@@ -1,6 +1,6 @@
 <script>
     //default values can be assigned as well
-    let {answer = 21, question = '3 * 7' , maker = 'teacher'} = $props();
+    let {...question} = $props();
 </script>
 
-<p>The question is {question}! The answer is {answer}. The question maker is {maker}!</p>
+<p>The question is {question.details}! The answer is {question.answer}. The question maker is {question.maker}!</p>
