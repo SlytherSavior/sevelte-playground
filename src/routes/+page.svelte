e27f688e2400da202bd7a13e3671b1325b9b8940
Shrijan Poudel <85357750+SlytherSavior@users.noreply.github.com>
2025-02-25 08:30:37 +0545

    basic svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<!-- sevelte doesnt provide any sanitization for html strings inserted into the DOM -->
+<script>
+    let string = 'This is a test html string <b>hiii hello</b>'
+</script>
+
+<p>{@html string}</p>
